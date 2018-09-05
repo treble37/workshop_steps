19f7ec9b3156eb0c93acfa9158dd62c5b4c2368c
Bruce Park <brucepark0@gmail.com>
2018-09-05 09:58:45 -0700

    Solution for TableProcessTwo

--- lib/table_process_two.ex
+++ lib/table_process_two.ex
@@ -1,9 +1,15 @@
 defmodule TableProcessTwo do
   def ping do
     # insert receive here
+    receive do
+      :ping -> IO.puts("received ping")
+    end
+
+    ping()
   end
 
   def start do
     # insert spawn here
+    spawn(__MODULE__, :ping, [])
   end
 end
