a404ea309a06adb0dae5e7be87aff1c9cb8c8154
Bruce Park <brucepark0@gmail.com>
2018-09-05 14:17:06 -0700

    Alternate syntax for TableSupervisorFour

--- lib/table_supervisor_four.ex
+++ lib/table_supervisor_four.ex
@@ -10,7 +10,11 @@ defmodule TableSupervisorFour do
     # Initialize children here
 
     # Now we start the supervisor with the children and a strategy
+    # Syntax 1:
     children = [{TableServerFour, 0}]
     Supervisor.init(children, strategy: :one_for_one)
+    # Syntax 2 (this is an old syntax):
+    # children = [worker(TableServerFour, [0])]
+    # supervise(children, strategy: :one_for_one)
   end
 end
