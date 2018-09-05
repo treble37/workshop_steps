5dc225b1765367b17ea86aa667156c69854fcb65
Bruce Park <brucepark0@gmail.com>
2018-09-05 11:51:04 -0700

    Add TableServerThree solution

--- lib/table_server_three.ex
+++ lib/table_server_three.ex
@@ -5,8 +5,9 @@ defmodule TableServerThree do
   # Client - API                              #
   # i.e. Client calls the following functions #
   # ----------------------------------------- #
-  def start_link(_) do
+  def start_link(arg_num) do
     # start link with initial value
+    GenServer.start_link(__MODULE__, arg_num, name: __MODULE__)
   end
 
   def init(args) do
@@ -15,6 +16,10 @@ defmodule TableServerThree do
 
   def ping() do
     GenServer.call(__MODULE__, :ping)
+  end
+
+  def pong() do
+    GenServer.call(__MODULE__, :pong)
   end
 
   # Be sure to setup a pong()
@@ -28,4 +33,7 @@ defmodule TableServerThree do
   end
 
   # pong handler goes here
+  def handle_call(:pong, _from, current_number) do
+    {:reply, {:ok, current_number}, current_number}
+  end
 end
