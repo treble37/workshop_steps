524ab12af412288a0d4e21ed9848f3a8e47152bd
Bruce Park <brucepark0@gmail.com>
2018-09-05 11:17:11 -0700

    Solution for TableServerOne

--- lib/table_server_one.ex
+++ lib/table_server_one.ex
@@ -3,9 +3,11 @@ defmodule TableServerOne do
 
   def init(args) do
     # init
+    {:ok, args}
   end
 
   def handle_call(:ping, _from, []) do
     # handle call and setup for next call
+    {:reply, {:ok}, []}
   end
 end
