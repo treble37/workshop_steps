15b40ab7e64fa2d4337aca137cd90e51503700a2
Bruce Park <brucepark0@gmail.com>
2018-09-05 09:50:16 -0700

    Solution for TableProcessOne

--- lib/table_process_one.ex
+++ lib/table_process_one.ex
@@ -1,9 +1,15 @@
 defmodule TableProcessOne do
   def ping do
     # insert receive(args) here
+    # once receive gets the message, it stops watching the mailbox
+    receive do
+      :ping -> IO.inspect("Hello :ping")
+      _ -> "Error"
+    end
   end
 
   def start do
     # insert spawn(fun) here
+    spawn(&TableProcessOne.ping/0)
   end
 end
